97245da9350ad08511162f5f83abb23ffa56eee3
Edu-Fortes <edu.mfortes@gmail.com>
2025-01-30 17:14:38 -0300

    Add event management with ASP.NET Core minimal API
    Introduce Events.DB namespace with Event record and EventDB class.
    EventDB manages a static list of events and provides CRUD methods.
    Update Program.cs to set up minimal web app with CRUD endpoints.

---
+++ EventsDB.cs
@@ -0,0 +1,55 @@
+﻿namespace Events.DB
+{
+    public record Event
+    {
+        public int Id { get; init; }
+        public string? Title { get; init; }
+        public string? Date { get; init; }
+        public bool AllDay { get; init; }
+        public string? Color { get; init; }
+        public string? TextColor { get; init; }
+    }
+
+    public class EventDB
+    {
+        private static List<Event> _events = new List<Event>()
+            {
+                new() { Id = 1, Title="Evento 1",Date="2025-01-29",AllDay=true,Color="red",TextColor="white"},
+                new() { Id = 2, Title="Evento 2",Date="2025-01-30",AllDay=true,Color="blue",TextColor="white"},
+                new() { Id = 3, Title="Evento 3",Date="2025-01-31",AllDay=true,Color="green",TextColor="white"},
+                new() { Id = 4, Title="Evento 4",Date="2025-02-01",AllDay=true,Color="yellow",TextColor="black"},
+            };
+
+        public static List<Event> GetEvents()
+        {
+            return _events;
+        }
+
+        public static Event? GetEvent(int id)
+        {
+            return _events.SingleOrDefault(e => e.Id == id); // e para evento. 'event' é uma palavra reservada no C#
+        }
+
+        public static Event CreateEvent(Event newEvent)
+        {
+            _events.Add(newEvent);
+            return newEvent;
+        }
+
+        public static Event UpdateEvent(Event updateEvent)
+        {
+            var existingEvent = _events.SingleOrDefault(e => e.Id == updateEvent.Id);
+            if (existingEvent != null)
+            {
+                _events.Remove(existingEvent);
+                _events.Add(updateEvent);
+            }
+            return updateEvent;
+        }
+
+        public static void DeleteEvent(int id)
+        {
+            _events = _events.FindAll(e => e.Id != id).ToList();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.Extensions.Logging;
+using Events.DB;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/events", () => EventDB.GetEvents());
+app.MapGet("/event/{id}", (int id) => EventDB.GetEvent(id));
+app.MapPost("/event", (Event newEvent) => EventDB.CreateEvent(newEvent));
+app.MapPut("/event", (Event updateEvent) => EventDB.UpdateEvent(updateEvent));
+app.MapDelete("/event/{id}", (int id) => EventDB.DeleteEvent(id));
 
 app.Run();
